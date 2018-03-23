af54ddc7bc83181e71dbd5b27f93358925946033
Patrick Laptop (MBP) <patirck.heenan@colorado.edu>
2018-03-22 19:03:25 -0600

    added plot info...

--- Code/Figures/ContirbutionOfPEG/main_contribution.py
+++ Code/Figures/ContirbutionOfPEG/main_contribution.py
@@ -13,6 +13,25 @@ sys.path.append("../../")
 from Lib.AppWLC.Code import WLC
 from Lib.UtilForce.UtilGeneral import PlotUtilities
 from scipy.integrate import cumtrapz
+
+class plot_info:
+    def __init__(self,ext,f,w):
+        self.q = ext
+        self.f = f
+        self.w = w
+
+def get_plot_info(ext,F,**kw):
+    ext_grid_final,force_grid_final,Force= \
+        WLC._inverted_wlc_full(ext=ext,F=F,odjik_as_guess=True,**kw)
+    ext = ext_grid_final
+    f = force_grid_final
+    work = cumtrapz(x=ext,y=f,initial=0)
+    # make out plot, units of nanometers, piconewtons, kcal/mol
+    ext_plot = ext * 1e9
+    f_plot = f * 1e12
+    w_plot = (work / 4.1e-21) * 0.593
+    return plot_info(ext_plot,f_plot,w_plot)
+
 def run():
     """
     <Description>
@@ -32,24 +51,15 @@ def run():
     K0 = 1000e-12
     kbT = 4.1e-21
     N_monomers = 77
-    L0_3400 = N_monomers * L0_per_monomer
-    ext_grid_final,force_grid_final,Force= \
-        WLC._inverted_wlc_full(ext=ext,kbT=kbT,Lp=Lp,L0=L0_3400,
-                               K0=K0,F=F,odjik_as_guess=True)
-    ext = ext_grid_final
-    f = force_grid_final
-    work = cumtrapz(x=ext,y=f,initial=0)
-    # make out plot, units of nanometers, piconewtons, kcal/mol
-    ext_plot = ext * 1e9
-    f_plot = f * 1e12
-    w_plot = (work / 4.1e-21) * 0.593
+    plot_inf = get_plot_info(ext=ext,F=F,Lp=Lp,K0=K0,kbT=kbT,
+                             L0=N_monomers * L0_per_monomer)
     fig = PlotUtilities.figure((2.5,4))
     ax1 = plt.subplot(2,1,1)
-    plt.plot(ext_plot,f_plot)
+    plt.plot(plot_inf.q,plot_inf.f)
     PlotUtilities.lazyLabel("","$F$ (pN)","")
     PlotUtilities.no_x_label(ax1)
     ax2 = plt.subplot(2,1,2)
-    plt.plot(ext_plot,w_plot,label="Total work")
+    plt.plot(plot_inf.q,plot_inf.w,label="Total work")
     PlotUtilities.lazyLabel("Extension (nm)","$W$ (kcal/mol)","")
     PlotUtilities.savefig(fig,"PEG.png")
 
