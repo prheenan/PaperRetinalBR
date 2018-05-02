1280ce78543abf35e7dca9db29fe2dc4e96eec3e
patrick.heenan@colorado.edu <patrick.heenan@colorado.edu>
2018-05-02 11:49:27 -0600

    updated subrepos, added a few files I didnt realize werent there.

---
+++ Code/Analysis/main/main.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# courtesy of: http://redsymbol.net/articles/unofficial-bash-strict-mode/
+# (helps with debugging)
+# set -e: immediately exit if we find a non zero
+# set -u: undefined references cause errors
+# set -o: single error causes full pipeline failure.
+set -euo pipefail
+IFS=$'\n\t'
+# datestring, used in many different places...
+dateStr=`date +%Y-%m-%d:%H:%M:%S`
+
+# Description:
+
+# Arguments:
+#### Arg 1: Description
+cd ../_0_landscape_alignment/
+ls
+echo $1
+python main_align_landscapes.py --base "$1"
+cd -
+
+# Returns:
+
+
+
