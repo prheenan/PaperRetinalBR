5fd52ee1974a1061932a2b16de718eaab5d49145
patrick.heenan@colorado.edu <patrick.heenan@colorado.edu>
2018-05-11 10:40:55 -0600

    added in reading of sample BO data.

--- Code/Figures/FigureUtil.py
+++ Code/Figures/FigureUtil.py
@@ -9,11 +9,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys
 
+sys.path.append("../")
+from Processing import ProcessingUtil
+
 from Lib.UtilPipeline import Pipeline
 from Lib.UtilForce.FEC import FEC_Util
-from Lib.UtilForce.UtilGeneral import CheckpointUtilities, GenUtilities
+from Lib.UtilForce.UtilGeneral import CheckpointUtilities, GenUtilities, \
+    PlotUtilities
 import RetinalUtil
-sys.path.append("../")
+
 
 class SnapshotFEC(object):
     def __init__(self,step,fec_list):
@@ -33,15 +37,32 @@ class AlignmentInfo(object):
         return to_ret
 
 class LandscapeGallery(object):
-    def __init__(self,PEG600,PEG3400):
+    def __init__(self,PEG600,PEG3400,BO_PEG3400):
         self.PEG600 = PEG600
         self.PEG3400 = PEG3400
+        self.BO_PEG3400 = BO_PEG3400
 
 def _snapsnot(base_dir,step):
     corrected_dir = Pipeline._cache_dir(base=base_dir,
                                         enum=step)
     data = CheckpointUtilities.lazy_multi_load(corrected_dir)
     return SnapshotFEC(step,data)
+
+def _plot_fmt(ax,xlim,ylim,is_bottom=False,color=False,is_left=True):
+    plt.xlim(xlim)
+    plt.ylim(ylim)
+    PlotUtilities.title("")
+    PlotUtilities.ylabel("$F$ (pN)")
+    if (not is_bottom):
+        PlotUtilities.no_x_label(ax=ax)
+        PlotUtilities.xlabel("")
+    if (not is_left):
+        PlotUtilities.no_y_label(ax=ax)
+        PlotUtilities.ylabel("")
+    if color:
+        color_kw = dict(ax=ax,color='w',label_color='k')
+        PlotUtilities.color_x(**color_kw)
+        PlotUtilities.color_y(**color_kw)
 
 
 def _alignment_pipeline(e):
@@ -57,19 +78,42 @@ def _alignment_pipeline(e):
     to_ret = AlignmentInfo(e,zeroed,polished,blacklist)
     return to_ret
 
+def _plot_fec_list(list_v,xlim,ylim,label=None,color=None,linewidth=0.3,**kw):
+    f_x = lambda x_tmp : x_tmp.Separation
+    for i,d in enumerate(list_v):
+        label_tmp = label if i == 0 else None
+        ProcessingUtil.plot_single_fec(d, f_x, xlim, ylim,label=label_tmp,
+                                       style_data=dict(color=color, alpha=0.3,
+                                                       linewidth=linewidth),
+                                       **kw)
+def _limits(_all_fecs):
+    xlim = [-20,120]
+    max_y = np.max([max(f.Force) for f in _all_fecs]) * 1e12
+    ylim = [-50,max(max_y,300)]
+    return xlim,ylim
+
+def _read_samples(base_dir_input,sample_names):
+    energies = RetinalUtil._read_all_energies(base_dir_input)
+    names = [e.base_dir.split("FECs180307")[1] for e in energies]
+    to_ret = []
+    for n in sample_names:
+        idx_tmp = names.index(n)
+        to_ret.append(energies[idx_tmp])
+    return to_ret
 
 def read_sample_landscapes(base_dir):
     """
     :param base_dir: input to RetinalUtil._read_all_energies
     :return:
     """
-    energies = RetinalUtil._read_all_energies(base_dir)
-    names = [e.base_dir.split("FECs180307")[1] for e in energies]
-    str_PEG600_example = "/BR+Retinal/300nms/170511FEC/landscape_"
-    str_PEG3400_example = "/BR+Retinal/3000nms/170503FEC/landscape_"
-    idx_PEG600 = names.index(str_PEG600_example)
-    idx_PEG3400 = names.index(str_PEG3400_example)
-    assert (idx_PEG600 >= 0) and (idx_PEG3400 >= 0)
-    to_ret = LandscapeGallery(PEG600=energies[idx_PEG600],
-                              PEG3400=energies[idx_PEG3400])
+    base_dir_BR = base_dir + "BR+Retinal/"
+    names_BR = ["/BR+Retinal/300nms/170511FEC/landscape_",
+                "/BR+Retinal/3000nms/170503FEC/landscape_"]
+    PEG600, PEG3400 = _read_samples(base_dir_BR,names_BR)
+    base_dir_BO = base_dir + "BR-Retinal/"
+    names_BO = ["/BR-Retinal/300nms/170327FEC/landscape_"]
+    BO_PEG3400 = _read_samples(base_dir_BO,names_BO)[0]
+    to_ret = LandscapeGallery(PEG600=PEG600,
+                              PEG3400=PEG3400,
+                              BO_PEG3400=BO_PEG3400)
     return to_ret
--- Code/Figures/SI/Alignment/main_alignment_demo.py
+++ Code/Figures/SI/Alignment/main_alignment_demo.py
@@ -15,7 +15,6 @@ from Lib.UtilPipeline import Pipeline
 from Lib.UtilForce.FEC import FEC_Util, FEC_Plot
 from Lib.UtilForce.UtilGeneral import CheckpointUtilities, GenUtilities
 from Lib.UtilForce.UtilGeneral import PlotUtilities
-from Processing import ProcessingUtil
 from Lib.AppWHAM.Code import WeightedHistogram, UtilWHAM
 import RetinalUtil,PlotUtil
 import matplotlib.gridspec as gridspec
@@ -25,55 +24,24 @@ from Figures import FigureUtil
 
 
 
-def _plot_fmt(ax,xlim,ylim,is_bottom=False,color=True,is_left=True):
-    plt.xlim(xlim)
-    plt.ylim(ylim)
-    PlotUtilities.title("")
-    PlotUtilities.ylabel("$F$ (pN)")
-    if (not is_bottom):
-        PlotUtilities.no_x_label(ax=ax)
-        PlotUtilities.xlabel("")
-    if (not is_left):
-        PlotUtilities.no_y_label(ax=ax)
-        PlotUtilities.ylabel("")
-    if color:
-        color_kw = dict(ax=ax,color='w',label_color='k')
-        PlotUtilities.color_x(**color_kw)
-        PlotUtilities.color_y(**color_kw)
-
-def _limits(alignment):
-    xlim = [-20,120]
-    max_y = np.max([max(f.Force) for f in alignment._all_fecs]) * 1e12
-    ylim = [-50,max(max_y,300)]
-    return xlim,ylim
-
-def _plot_fec_list(list_v,xlim,ylim,label=None,color=None,linewidth=0.3,**kw):
-    f_x = lambda x_tmp : x_tmp.Separation
-    for i,d in enumerate(list_v):
-        label_tmp = label if i == 0 else None
-        ProcessingUtil.plot_single_fec(d, f_x, xlim, ylim,label=label_tmp,
-                                       style_data=dict(color=color, alpha=0.3,
-                                                       linewidth=linewidth),
-                                       **kw)
-
 def _ensemble_alignment(gs,alignment,col_idx):
-    xlim, ylim = _limits(alignment)
+    xlim, ylim = FigureUtil._limits(alignment._all_fecs)
     common_kw = dict(xlim=xlim,ylim=ylim)
     kw_fmt = dict(color=False,is_left=(col_idx ==0),**common_kw)
     ax1 = plt.subplot(gs[0,col_idx])
-    _plot_fec_list(alignment.zeroed.fec_list,**common_kw)
-    _plot_fmt(ax1,**kw_fmt)
+    FigureUtil._plot_fec_list(alignment.zeroed.fec_list,**common_kw)
+    FigureUtil._plot_fmt(ax1,**kw_fmt)
     ax2 = plt.subplot(gs[1,col_idx])
-    _plot_fec_list(alignment.polished.fec_list,**common_kw)
-    _plot_fmt(ax2,**kw_fmt)
+    FigureUtil._plot_fec_list(alignment.polished.fec_list,**common_kw)
+    FigureUtil._plot_fmt(ax2,**kw_fmt)
     PlotUtilities.no_x_label(ax=ax2)
     ax3 = plt.subplot(gs[2,col_idx])
-    _plot_fec_list(alignment.blacklisted.fec_list,**common_kw)
-    _plot_fmt(ax3,is_bottom=True,**kw_fmt)
+    FigureUtil._plot_fec_list(alignment.blacklisted.fec_list,**common_kw)
+    FigureUtil._plot_fmt(ax3,is_bottom=True,**kw_fmt)
 
 
 def _heatmap_alignment(gs,alignment,col_idx):
-    xlim, ylim = _limits(alignment)
+    xlim, ylim = FigureUtil._limits(alignment._all_fecs)
     max_x = xlim[1]
     bin_step_nm = 1
     bin_step_pN = 5
@@ -83,10 +51,10 @@ def _heatmap_alignment(gs,alignment,col_idx):
                      bins=(bins_x,bins_y))
     ax1 = plt.subplot(gs[0,col_idx])
     FEC_Plot.heat_map_fec(alignment.zeroed.fec_list,**common_kw)
-    _plot_fmt(ax1,xlim,ylim)
+    FigureUtil._plot_fmt(ax1,xlim,ylim,color=True)
     ax2 = plt.subplot(gs[1,col_idx])
     FEC_Plot.heat_map_fec(alignment.polished.fec_list,**common_kw)
-    _plot_fmt(ax2,xlim,ylim)
+    FigureUtil._plot_fmt(ax2,xlim,ylim,color=True)
     title_kw = dict(color='b',y=0.95,loc='left',fontsize=6)
     downarrow = "$\Downarrow$"
     title_sub = downarrow + " Subtract $X_{\mathbf{PEG3400}}(F)$ + " + \
@@ -95,7 +63,7 @@ def _heatmap_alignment(gs,alignment,col_idx):
     PlotUtilities.no_x_label(ax=ax2)
     ax3 = plt.subplot(gs[2,col_idx])
     FEC_Plot.heat_map_fec(alignment.blacklisted.fec_list,**common_kw)
-    _plot_fmt(ax3,xlim,ylim,True)
+    FigureUtil._plot_fmt(ax3,xlim,ylim,is_bottom=True,color=True)
     PlotUtilities.title(downarrow + " Remove poorly-fit FECs",**title_kw)
     return [ax1,ax2,ax3]
 
@@ -120,7 +88,7 @@ def _make_plots(galleries_labels):
         alignment = alignments[i]
         _make_algned_plot(alignment, label)
     # plot the final curves on the same plot
-    xlim,ylim = _limits(alignment)
+    xlim,ylim = FigureUtil._limits(alignment._all_fecs)
     colors = ['rebeccapurple','g']
     fig = PlotUtilities.figure((5, 3))
     gs = gridspec.GridSpec(2, 2)
@@ -130,8 +98,8 @@ def _make_plots(galleries_labels):
     for i,(_,l) in enumerate(galleries_labels):
         ax = plt.subplot(gs[i,0])
         a = alignments[i]
-        _plot_fec_list(a.blacklisted.fec_list, xlim, ylim,label=l,
-                       color=colors[i])
+        FigureUtil._plot_fec_list(a.blacklisted.fec_list, xlim, ylim,label=l,
+                                  color=colors[i])
         if (i == 0):
             PlotUtilities.no_x_label(ax)
             PlotUtilities.xlabel("",ax=ax)
@@ -140,8 +108,8 @@ def _make_plots(galleries_labels):
     kw = [dict(),dict(linewidth=0.6)]
     for i,(_,l) in enumerate(galleries_labels):
         a = alignments[i]
-        _plot_fec_list(a.blacklisted.fec_list, xlim, ylim,label=l,
-                       color=colors[i],**kw[i])
+        FigureUtil._plot_fec_list(a.blacklisted.fec_list, xlim, ylim,label=l,
+                                  color=colors[i],**kw[i])
     PlotUtilities.savefig(fig,"FigureS_3400vs600.png")
 
 
@@ -156,10 +124,9 @@ def run():
         This is a description of what is returned.
     """
     base_dir = "../../../../Data/FECs180307/"
-    base_dir_input = base_dir + "BR+Retinal/"
     read_f = FigureUtil.read_sample_landscapes
     gallery = CheckpointUtilities.getCheckpoint("./caches.pkl",
-                                                read_f,True,base_dir_input)
+                                                read_f,True,base_dir)
     galleries_labels = [ [gallery.PEG600,"PEG600"],
                          [gallery.PEG3400, "PEG3400"]]
     _make_plots(galleries_labels)
