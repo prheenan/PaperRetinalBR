ad3ed3e324cabb4899bca430d87593c290063117
patrick.heenan@colorado.edu <patrick.heenan@colorado.edu>
2018-05-11 13:25:55 -0600

    betgter pltting, sent these to david and devin.

--- Code/Figures/SI/RawWork/main_work_demo.py
+++ Code/Figures/SI/RawWork/main_work_demo.py
@@ -59,6 +59,22 @@ def _mean_work(x_arr,works_kcal):
     std_W = np.std(interp_W,axis=0)
     return x_interp, mean_W, std_W
 
+def xlim_ylim():
+    ylim_work = [-20,750]
+    xlim = [20,60]
+    ylim_force = [-20,300]
+    return xlim, ylim_force,ylim_work
+
+def _plot_mean_works(x_interp,mean_W,std_W,color,title):
+    x_interp_plot = x_interp * 1e9
+    plt.errorbar(x_interp_plot,mean_W,fmt='-',color=color,markersize=2,
+                 capsize=2)
+    label_W = title.split("-PEG")[0]
+    label_W_mean = r"$(\mu  \pm \sigma)" + \
+                   r"_{W,\mathbf{" + label_W + r"}}$"
+    plt.fill_between(x_interp_plot,mean_W-std_W,mean_W+std_W,color=color,
+                     alpha=0.3,label=label_W_mean,linewidth=0)
+
 def _make_work_plot(fec_list,x_arr,works_kcal,gs,col,color,title):
     # get the interpolated work
     x_interp, mean_W, std_W = _mean_work(x_arr, works_kcal)
@@ -70,19 +86,17 @@ def _make_work_plot(fec_list,x_arr,works_kcal,gs,col,color,title):
     legend_kw = dict(handlelength=1.5,handletextpad=0.3,fontsize=6)
     david_F = _f_david(kbT=4.1e-21, L0=11e-9+shift, Lp=0.4e-9, x=x_david)
     david_W = _single_work(x=x_david,f=david_F)
+    xlim, ylim_force, ylim_work = xlim_ylim()
     x_david_plot = x_david * 1e9
     W_david_plot = david_W * kcal_per_mol_per_J()
     f_david_plot = david_F * 1e12
     is_left = (col == 0)
     fmt_kw = dict(is_left=is_left)
-    label_work = "W (kcal/mol)"
+    label_work = "$W$ (kcal/mol)"
     # interpolate each work onto a grid
     _,ylim = FigureUtil._limits(fec_list)
     xlim = [20,60]
-    x_interp_plot = x_interp * 1e9
     fudge_work = max(std_W)
-    max_W = np.max([max(w) for w in works_kcal])+fudge_work
-    ylim_work = [-20,750]
     ax1 = plt.subplot(gs[0,col])
     FigureUtil._plot_fec_list(fec_list,xlim,ylim)
     plt.plot(x_david_plot,f_david_plot,**style_david)
@@ -95,18 +109,12 @@ def _make_work_plot(fec_list,x_arr,works_kcal,gs,col,color,title):
         plt.plot(x * 1e9,w,linewidth=0.75)
     FigureUtil._plot_fmt(ax2, xlim, ylim_work,ylabel=label_work,**fmt_kw)
     ax3 = plt.subplot(gs[2,col])
-    plt.errorbar(x_interp_plot,mean_W,fmt='-',color=color,markersize=2,
-                 capsize=2)
-    label_W = title.split("-PEG")[0]
-    label_W_mean = r"$(\mu  \pm \sigma)" + \
-                   r"_\mathbf{W, " + label_W + r"}$"
-    plt.fill_between(x_interp_plot,mean_W-std_W,mean_W+std_W,color=color,
-                     alpha=0.3,label=label_W_mean,linewidth=0)
+    _plot_mean_works(x_interp, mean_W, std_W, color, title)
     style_lower_david = dict(**style_david)
     if (not is_left):
         style_lower_david['label'] = None
     plt.plot(x_david_plot,W_david_plot,'b--',zorder=5,**style_lower_david)
-    PlotUtilities.legend(loc='lower right',**legend_kw)
+    PlotUtilities.legend(**legend_kw)
     FigureUtil._plot_fmt(ax3, xlim, ylim_work,is_bottom=True,
                          ylabel=label_work,**fmt_kw)
 
@@ -126,9 +134,14 @@ def run():
                                                 read_f,False,base_dir)
     lab_plot = [ ["BR-PEG3400",gallery.PEG3400],
                  ["BO-PEG3400",gallery.BO_PEG3400] ]
-    gs = gridspec.GridSpec(ncols=2,nrows=4)
     fig = PlotUtilities.figure(figsize=(4,6))
+    gs = gridspec.GridSpec(ncols=1,nrows=2,height_ratios=[3,1])
+    gs_pipeline= gridspec.GridSpecFromSubplotSpec(ncols=2,nrows=3,hspace=0.03,
+                                                  subplot_spec=gs[0,:])
     colors = ['r','rebeccapurple']
+    xlim,_,ylm_W = xlim_ylim()
+
+    mean_works_info = []
     for i,(label,to_use) in enumerate(lab_plot):
         pipeline = FigureUtil._alignment_pipeline(to_use)
         fecs = pipeline.blacklisted.fec_list
@@ -137,8 +150,16 @@ def run():
         f_arr = [f.Force for f in fecs]
         works = _calculate_work(x_arr,f_arr)
         works_kcal = np.array(works) * kcal_per_mol_per_J()
-        _make_work_plot(fecs, x_arr, works_kcal,gs,col=i,color=colors[i],
-                        title=label)
+        c = colors[i]
+        _make_work_plot(fecs, x_arr, works_kcal,gs_pipeline,col=i,color=c,
+                         title=label)
+        x_interp, mean_W, std_W= _mean_work(x_arr,works_kcal)
+        plt.subplot(gs[-1, :])
+        _plot_mean_works(x_interp, mean_W, std_W, color=c, title=label)
+        plt.xlim(xlim)
+        plt.ylim(ylm_W)
+        PlotUtilities.lazyLabel("Extension (nm)" , "$W$ (kcal/mol)","",
+                                useLegend=False)
     PlotUtilities.savefig(fig, "FigureS_Work.png".format(label))
 
 
