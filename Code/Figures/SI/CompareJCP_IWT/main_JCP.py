ad143e8e75f648f277e779eaa045e6381f7b73f1
patrick.heenan@colorado.edu <patrick.heenan@colorado.edu>
2018-05-14 16:08:37 -0600

    added jcp file immediately after last commit. looks like second derivative is maybe a bit messed up..

---
+++ Code/Figures/SI/CompareJCP_IWT/main_JCP.py
@@ -0,0 +1,77 @@
+# force floating point division. Can still use integer with //
+from __future__ import division
+# other good compatibility recquirements for python3
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import unicode_literals
+# This file is used for importing the common utilities classes.
+import numpy as np
+import matplotlib.pyplot as plt
+import sys, os
+import re
+
+sys.path.append("../../../")
+from Lib.UtilPipeline import Pipeline
+from Lib.UtilForce.FEC import FEC_Util, FEC_Plot
+from Lib.UtilForce.UtilGeneral import CheckpointUtilities, GenUtilities
+from Lib.UtilForce.UtilGeneral import PlotUtilities
+from Lib.UtilForce.UtilGeneral.Plot import Scalebar
+
+from Processing import ProcessingUtil
+from Lib.AppWHAM.Code import WeightedHistogram, UtilWHAM
+import RetinalUtil,PlotUtil
+import matplotlib.gridspec as gridspec
+from Processing.Util import WLC
+from Figures import FigureUtil
+
+def run():
+    """
+    <Description>
+
+    Args:
+        param1: This is the first param.
+    
+    Returns:
+        This is a description of what is returned.
+    """
+    input_dir = "../../../../Data/FECs180307/"
+    out_dir = "./"
+    q_offset_nm = RetinalUtil.min_sep_landscape() * 1e9
+    min_fecs = 8
+    q_interp, energy_list_arr = FigureUtil.\
+        _read_energy_list_and_q_interp(input_dir, q_offset=q_offset_nm,
+                                       min_fecs=min_fecs,remove_noisy=True)
+    ex = energy_list_arr[0][0]
+    q_start_nm = np.array([e._landscape.q_nm[0] for e in ex._other_helices])
+    q_target_nm = 31
+    helix_idx = np.argmin(np.abs(q_start_nm - q_target_nm))
+    helix = ex._other_helices[helix_idx]
+    landscape = helix._landscape
+    data = RetinalUtil.read_fecs(ex)
+    _, data_sliced = RetinalUtil.slice_data_for_helix(data,
+                                                      min_ext_m=helix.min_ext_m)
+    # XXX why is this necessary?? screwing up absolute values
+    previous_JCP = FigureUtil.read_non_peg_landscape(base="../../FigData/")
+    offset_s = np.mean([d.Separation[0] for d in data_sliced])
+    offset_jcp_nm = -45
+    offset_jcp_kcal = 40
+    landscape_offset_nm = (landscape.q_nm[0]-offset_s * 1e9)
+    fig = PlotUtilities.figure()
+    xlim, _ = FigureUtil._limits(data)
+    fmt = dict(xlim=xlim,ylim=[None,None])
+    ax1 = plt.subplot(2,1,1)
+    FigureUtil._plot_fec_list(data,color='k',**fmt)
+    FigureUtil._plot_fec_list(data_sliced,**fmt)
+    FigureUtil._plot_fmt(ax1, **fmt)
+    ax2 = plt.subplot(2,1,2)
+    plt.plot(landscape.q_nm-landscape_offset_nm,landscape.G0_kcal_per_mol)
+    plt.plot(previous_JCP.q_nm-offset_jcp_nm,
+             previous_JCP.G0_kcal_per_mol-offset_jcp_kcal,'r--')
+    FigureUtil._plot_fmt(ax2, ylabel="G (kcal.mol)",is_bottom=True,**fmt)
+    PlotUtilities.savefig(fig,"./out.png")
+    pass
+
+
+
+if __name__ == "__main__":
+    run()
